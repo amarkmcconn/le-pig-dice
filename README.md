472c48227c48cac36c0dd1d7fbbc09dc8352d290
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-04-06 11:41:16 -0700

    WIP js UI
    Co-authored-by: Mark McConnell <amark.mcconnell@gmail.com>

---
+++ README.md
@@ -0,0 +1,26 @@
+```javascript
+
+Description: rollDice()
+
+Test 1:
+Randomly return a number between 1 and 6
+Expected Output: 2
+
+Test 2:
+Stop rolling dice if user rolls a 1
+Expected Output: false  (next players turn)
+
+Description: holdScore()
+Test 1:
+holds totalScore
+
+Test 2:
+holds totalScore
+zeroes out roundscore
+
+Player 1 & Player 2 
+Object to hold 
+turn score
+total score
+
+```
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
+// Business Logic
+
 function Player(id) {
   this.roundScore = 0;
   this.totalScore = 0;
   this.id = id;
 }
-
 
 Player.prototype.rollDice = function() {
   let roll = Math.floor((Math.random()*6) + 1);
@@ -15,4 +16,33 @@ Player.prototype.rollDice = function() {
   return [roll, this.roundScore];
 }
 
+Player.prototype.holdScore = function() {
+  this.totalScore += this.roundScore;
+  this.roundScore = 0;
+  return this.totalScore;
+}
 
+
+// UI Logic
+let player1 = new Player(1);
+let player2 = new Player(2);
+
+
+$(document).ready(function () {
+  $(".player1Roll").click(function (event) {
+    event.preventDefault();
+    let roll1 = player1.rollDice();
+    $("#roll1").text(roll1[0]);
+    $("#roundScore1").text(roll1[1]);
+    $(".player2Roll").hide();
+  });
+});
+
+$(document).ready(function () {
+  $(".player2Roll").click(function (event) {
+    event.preventDefault();
+    let roll2 = player2.rollDice();
+    $("#roll2").text(roll2[0]);
+    $("#roundScore2").text(roll2[1]);
+  });
+});
