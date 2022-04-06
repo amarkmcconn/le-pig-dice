a6926e1fe7445cd77e55730c3ea5d7782b0d5382
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-04-06 13:56:17 -0700

    finsih js BL and js UI
    Co-authored-by: Mark McConnell <amark.mcconnell@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,7 @@ Player.prototype.holdScore = function() {
 
 
 // UI Logic
+
 let player1 = new Player(1);
 let player2 = new Player(2);
 
@@ -34,15 +35,51 @@ $(document).ready(function () {
     let roll1 = player1.rollDice();
     $("#roll1").text(roll1[0]);
     $("#roundScore1").text(roll1[1]);
-    $(".player2Roll").hide();
+    if (roll1[0] === 1) {
+      $(".player1Roll").hide();
+      $(".player1Hold").hide();
+      $("#player2Hold").show();
+      $(".player2Roll").show();
+    }
   });
-});
 
-$(document).ready(function () {
   $(".player2Roll").click(function (event) {
-    event.preventDefault();
     let roll2 = player2.rollDice();
     $("#roll2").text(roll2[0]);
     $("#roundScore2").text(roll2[1]);
+    if (roll2[0] === 1) {
+      $(".player2Roll").hide();
+      $("#player2Hold").hide();
+      $(".player1Hold").show();
+      $(".player1Roll").show();
+    }
+  });
+
+  $(".player1Hold").click(function (event) {
+    $("#totalScore1").text(player1.holdScore());
+    $("#roll1").text("");
+    $("#roundScore1").text("");
+    $(".player1Roll").hide();
+    $(".player1Hold").hide();
+    $("#player2Hold").show();
+    $(".player2Roll").show();
+    if (player1.totalScore >= 100) {
+      $("#winner1").append("WINNER, WINNER, WINNER!");
+      $("#winner2").append("LOSER, LOSER, LOSER!");
+    }
+  });
+
+  $("#player2Hold").click(function (event) {
+    $("#totalScore2").text(player2.holdScore());
+    $("#roll2").text("");
+    $("#roundScore2").text("");
+    $(".player2Roll").hide();
+    $("#player2Hold").hide();
+    $(".player1Hold").show();
+    $(".player1Roll").show();
+    if (player2.totalScore >= 100) {
+      $("#winner2").append("WINNER, WINNER, WINNER!");
+      $("#winner1").append("LOSER, LOSER, LOSER!");
+    }
   });
 });
